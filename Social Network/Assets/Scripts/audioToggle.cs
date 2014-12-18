cb1217272f6ebe8d420fa8887715d34a7d1acd77
Nick Hester <nick_hester@hotmail.com>
2014-12-17 23:53:22 -0600

    added persistent audio toggle

---
+++ Social Network/Assets/Scripts/audioToggle.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class audioToggle : MonoBehaviour {
+
+	public Material matOn;
+	public Material matOff;
+
+	private bool musicIsOn;
+
+	// Use this for initialization
+	void Start () {
+
+		GameObject foundSelf = GameObject.Find(gameObject.name);
+		
+		if (foundSelf != null && foundSelf != gameObject)
+		{
+			Destroy(gameObject);
+		}
+		DontDestroyOnLoad(gameObject);
+
+		if (SaveGame.GetAudioOn() == true)
+		{
+			gameObject.renderer.material = matOn;
+			musicIsOn = true;
+		}
+		else
+		{
+			gameObject.renderer.material = matOff;
+			musicIsOn = false;
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButtonDown(0))
+		{
+			if (getObjectAtMouse() == gameObject)
+			{
+				if (musicIsOn == true)
+				{
+					SaveGame.SetAudioOn(false);
+					gameObject.renderer.material = matOff;
+					musicIsOn = false;
+
+					AudioSource audio = GameObject.Find("Music Player").GetComponent<AudioSource>();
+					audio.enabled = false;
+				}
+				else
+				{
+					SaveGame.SetAudioOn(true);
+					gameObject.renderer.material = matOn;
+					musicIsOn = true;
+
+					AudioSource m_audio = GameObject.Find("Music Player").GetComponent<AudioSource>();
+					m_audio.enabled = true;
+					m_audio.Play();
+				}
+			}
+		}
+	
+	}
+
+
+	GameObject getObjectAtMouse()
+	{
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hit;
+		if (Physics.Raycast (ray, out hit, 10.0f))
+		{
+			return hit.transform.gameObject;
+		}
+		return null;
+	}
+}
