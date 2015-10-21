baa423583e7c3b512281240a9aa40d2a6335061d
Nick Hester <nick_hester@hotmail.com>
2015-10-21 12:48:06 -0500

    Revert "Initial attempt with Google Analytics"
    This reverts commit 6fd39514ce7b7007b56a9907ad1e8437e67db5c9.

--- Social Network/Assets/Scripts/MetricsLogger.cs
+++ Social Network/Assets/Scripts/MetricsLogger.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 //using GameAnalyticsSDK;
-//using UnityEngine.Analytics;
+using UnityEngine.Analytics;
 
 public class MetricsLogger : MonoBehaviour {
 
@@ -22,18 +22,15 @@ public class MetricsLogger : MonoBehaviour {
             return instance;
         }
     }
-	public GoogleAnalyticsV4 googleAnalytics;
 
 	public void SendDataOnStart()
 	{
 		//Analytics.SetUserGender(Gender.Male);
 		//Analytics.SetUserBirthYear(1990);
-		googleAnalytics.StartSession();
 	}
 
 	public void LogOnAppointmentEnd(int day, int appointment, int numActions, int numStarsEarned, float timeTaken)
 	{
-		/*
 		Analytics.CustomEvent("AppointmentInfo", new Dictionary<string, object>()
 		{
 			{ "appointment", ("" + day + "." + appointment) },
@@ -41,66 +38,46 @@ public class MetricsLogger : MonoBehaviour {
 			{ "numStarsEarned", numStarsEarned },
 			{ "timeTaken", timeTaken }
 		});
-		*/
-
-		EventHitBuilder ehb = new EventHitBuilder();
-		ehb.SetEventCategory("Appt");
-		ehb.SetEventAction("Success");
-		ehb.SetEventLabel(("" + day + "." + appointment));
-		ehb.SetEventValue(numStarsEarned);
-		ehb.SetCustomMetric(1, "" + numActions);
-		ehb.SetCustomMetric(2, "" + timeTaken);
-		googleAnalytics.LogEvent(ehb);
 	}
 
 	public void LogOnSessionEnd(int _appointments, float _length)
 	{
-		/*
 		Analytics.CustomEvent("SessionEnd", new Dictionary<string, object>()
 		{
 			{ "numAppointments", _appointments },
 			{ "sessionLength", _length }
 		});
-		*/
 	}
 
 	public void LogCustomEvent(string name, int day, int appointment)
 	{
-		/*
 		Analytics.CustomEvent(name, new Dictionary<string, object>()
 		{
 			{ "appointment", ("" + day + "." + appointment) }
 		});
-		 */
 	}
 
 	//=====================================================
-	
+	/*
 	public void LogCustomEvent(string _01, string _02, string _03, float score)
 	{
 		//GameAnalytics.NewDesignEvent(_01 + ":" + _02 + ":" + _03, score);
 	}
-	
+	*/
 	public void LogCustomEvent(string _01, string _02, string _03)
 	{
 		//GameAnalytics.NewDesignEvent(_01 + ":" + _02 + ":" + _03);
-
-		/*
 		Analytics.CustomEvent(_01 + "." + _02 + "." + _03, new Dictionary<string, object>()
 		{
 			{ "data", 0 }
 		});
-		 */
 	}
 	
 	public void LogBusinessEvent(string _currency, int _amountInCents, string _itemType, string _itemId, string _cartType, string _receipt, string _signature)
 	{
 		// if you switch back to GameAnalytics, then you need to convert the currency to number of cents
 		//GameAnalytics.NewBusinessEventGooglePlay(_currency, _amountInCents, _itemType, _itemId, _cartType, _receipt, _signature);
-
-		/*
 		Analytics.Transaction(_itemType, ((decimal)_amountInCents / 100.0m), "USD", _receipt, _signature);
-		 */
 	}
 
 	public void LogProgressionEvent_Complete(string _01)
