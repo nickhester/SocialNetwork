eb3750bb42fe442e539c66bfcce17c0f458fb273
Nick Hester <nick_hester@hotmail.com>
2016-10-27 17:08:04 -0500

    some cleanup on localization

--- Social Network/Assets/Scripts/LocalizedText.cs
+++ Social Network/Assets/Scripts/LocalizedText.cs
@@ -16,5 +16,14 @@ public class LocalizedText : MonoBehaviour
 		Text myTextObject = GetComponent<Text>();
 		LocalizedTextManager localizedTextManager = FindObjectOfType<LocalizedTextManager>();
 		myTextObject.text = localizedTextManager.GetLocalizedString(stringName);
+
+		if (localizedTextManager.currentLanguage == LocalizedTextManager.Language.English)
+		{
+			myTextObject.resizeTextForBestFit = false;
+		}
+		else
+		{
+			myTextObject.resizeTextForBestFit = true;
+		}
 	}
 }
